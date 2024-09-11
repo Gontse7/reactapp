4c2fa13f4fabf59722019b8ab37c17dad2595dcf
Gontse7 <gontsemogana7@gmail.com>
2024-09-12 00:23:09 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Weather App</h1>  
-        
+      <div className="container">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by Gontse H Mogana with an{" "}
+        <a href="https://github.com/Gontse7/reactapp" target="_blank" rel="noreferrer">open source</a> code.
+      </footer>
+      </div>
     </div>
   );
 }
